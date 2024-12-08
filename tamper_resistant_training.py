1fc06985e6f52fee9616bec62ead59f324b81f92
Garvit Banga <37771583+GarvitBanga@users.noreply.github.com>
2024-12-08 17:30:47 -0500

    Create tamper_resistant_training.py

---
+++ tamper_resistant_training.py
@@ -0,0 +1,307 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import json
+import os
+import sys
+from copy import deepcopy
+from omegaconf import OmegaConf
+from pathlib import Path
+from typing import Callable, Iterable
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+
+import utils
+import utils_img
+import utils_model
+sys.path.append('stablesignature/')
+sys.path.append('stablesignature/src/')
+sys.path.append('stablesignature/src/ldm/')
+from src.ldm.models.autoencoder import AutoencoderKL
+from src.ldm.models.diffusion.ddpm import LatentDiffusion
+from src.loss.loss_provider import LossProvider
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+
+    def aa(*args, **kwargs):
+        group.add_argument(*args, **kwargs)
+
+    group = parser.add_argument_group('Data parameters')
+    aa("--train_dir", type=str, help="Path to the training data directory", required=True)
+    aa("--val_dir", type=str, help="Path to the validation data directory", required=True)
+
+    group = parser.add_argument_group('Model parameters')
+    aa("--ldm_config", type=str, default="sd/stable-diffusion-v-1-4-original/v1-inference.yaml", help="Path to the configuration file for the LDM model") 
+    aa("--ldm_ckpt", type=str, default="sd/stable-diffusion-v-1-4-original/sd-v1-4-full-ema.ckpt", help="Path to the checkpoint file for the LDM model") 
+    aa("--msg_decoder_path", type=str, default= "models/hidden/dec_48b_whit.torchscript.pt", help="Path to the hidden decoder for the watermarking model")
+    aa("--num_bits", type=int, default=48, help="Number of bits in the watermark")
+    aa("--redundancy", type=int, default=1, help="Number of times the watermark is repeated to increase robustness")
+    aa("--decoder_depth", type=int, default=8, help="Depth of the decoder in the watermarking model")
+    aa("--decoder_channels", type=int, default=64, help="Number of channels in the decoder of the watermarking model")
+
+    group = parser.add_argument_group('Training parameters')
+    aa("--batch_size", type=int, default=4, help="Batch size for training")
+    aa("--img_size", type=int, default=256, help="Resize images to this size")
+    aa("--loss_i", type=str, default="watson-vgg", help="Type of loss for the image loss. Can be watson-vgg, mse, watson-dft, etc.")
+    aa("--loss_w", type=str, default="bce", help="Type of loss for the watermark loss. Can be mse or bce")
+    aa("--lambda_i", type=float, default=0.2, help="Weight of the image loss in the total loss")
+    aa("--lambda_w", type=float, default=1.0, help="Weight of the watermark loss in the total loss")
+    aa("--optimizer", type=str, default="AdamW,lr=5e-4", help="Optimizer and learning rate for training")
+    aa("--steps", type=int, default=100, help="Number of steps to train the model for")
+    aa("--warmup_steps", type=int, default=20, help="Number of warmup steps for the optimizer")
+
+    group = parser.add_argument_group('Logging and saving freq. parameters')
+    aa("--log_freq", type=int, default=10, help="Logging frequency (in steps)")
+    aa("--save_img_freq", type=int, default=1000, help="Frequency of saving generated images (in steps)")
+
+    group = parser.add_argument_group('Experiments parameters')
+    aa("--num_keys", type=int, default=1, help="Number of fine-tuned checkpoints to generate")
+    aa("--output_dir", type=str, default="output/", help="Output directory for logs and images (Default: /output)")
+    aa("--seed", type=int, default=0)
+    aa("--debug", type=utils.bool_inst, default=False, help="Debug mode")
+    aa("--strategy", type=int, default=0, help="Watermarking strategy (0: original, 1: randomfrompaper, 2: gradual randomness)")
+    aa("--outer_steps", type=int, default=100,help="Outer loop steps")
+    aa("--inner_steps", type=int, default=10,help="Inner loop steps")
+
+    return parser
+
+
+def main(params):
+
+    # Set seeds for reproductibility 
+    torch.manual_seed(params.seed)
+    torch.cuda.manual_seed_all(params.seed)
+    np.random.seed(params.seed)
+    
+    # Print the arguments
+    print("__git__:{}".format(utils.get_sha()))
+    print("__log__:{}".format(json.dumps(vars(params))))
+
+    # Create the directories
+    if not os.path.exists(params.output_dir):
+        os.makedirs(params.output_dir)
+    imgs_dir = os.path.join(params.output_dir, 'imgs')
+    params.imgs_dir = imgs_dir
+    if not os.path.exists(imgs_dir):
+        os.makedirs(imgs_dir, exist_ok=True)
+
+    # Loads LDM auto-encoder models
+    print(f'>>> Building LDM model with config {params.ldm_config} and weights from {params.ldm_ckpt}...')
+    config = OmegaConf.load(f"{params.ldm_config}")
+    ldm_ae: LatentDiffusion = utils_model.load_model_from_config(config, params.ldm_ckpt)
+    ldm_ae: AutoencoderKL = ldm_ae.first_stage_model
+    if(params.strategy!=0):
+        state_dict = torch.load("output/checkpoint_000.pth")['ldm_decoder']
+        msg = ldm_ae.first_stage_model.load_state_dict(state_dict, strict=False)
+        print(f"loaded LDM decoder state_dict with message\n{msg}")
+        print("you should check that the decoder keys are correctly matched")
+    ldm_ae.eval()
+    ldm_ae.to(device)
+    
+    # Loads hidden decoder
+    print(f'>>> Building hidden decoder with weights from {params.msg_decoder_path}...')
+    if 'torchscript' in params.msg_decoder_path: 
+        msg_decoder = torch.jit.load(params.msg_decoder_path).to(device)
+        # already whitened
+        
+    else:
+        msg_decoder = utils_model.get_hidden_decoder(num_bits=params.num_bits, redundancy=params.redundancy, num_blocks=params.decoder_depth, channels=params.decoder_channels).to(device)
+        ckpt = utils_model.get_hidden_decoder_ckpt(params.msg_decoder_path)
+        print(msg_decoder.load_state_dict(ckpt, strict=False))
+        msg_decoder.eval()
+
+        # whitening
+        print(f'>>> Whitening...')
+        with torch.no_grad():
+            # features from the dataset
+            transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(256),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+            loader = utils.get_dataloader(params.train_dir, transform, batch_size=16, collate_fn=None)
+            ys = []
+            for i, x in enumerate(loader):
+                x = x.to(device)
+                y = msg_decoder(x)
+                ys.append(y.to('cpu'))
+            ys = torch.cat(ys, dim=0)
+            nbit = ys.shape[1]
+            
+            # whitening
+            mean = ys.mean(dim=0, keepdim=True) # NxD -> 1xD
+            ys_centered = ys - mean # NxD
+            cov = ys_centered.T @ ys_centered
+            e, v = torch.linalg.eigh(cov)
+            L = torch.diag(1.0 / torch.pow(e, exponent=0.5))
+            weight = torch.mm(L, v.T)
+            bias = -torch.mm(mean, weight.T).squeeze(0)
+            linear = nn.Linear(nbit, nbit, bias=True)
+            linear.weight.data = np.sqrt(nbit) * weight
+            linear.bias.data = np.sqrt(nbit) * bias
+            msg_decoder = nn.Sequential(msg_decoder, linear.to(device))
+            torchscript_m = torch.jit.script(msg_decoder)
+            params.msg_decoder_path = params.msg_decoder_path.replace(".pth", "_whit.pth")
+            print(f'>>> Creating torchscript at {params.msg_decoder_path}...')
+            torch.jit.save(torchscript_m, params.msg_decoder_path)
+    
+    msg_decoder.eval()
+    nbit = msg_decoder(torch.zeros(1, 3, 128, 128).to(device)).shape[-1]
+
+    # Freeze LDM and hidden decoder
+    for param in [*msg_decoder.parameters(), *ldm_ae.parameters()]:
+        param.requires_grad = False
+
+    # Loads the data
+    print(f'>>> Loading data from {params.train_dir} and {params.val_dir}...')
+    vqgan_transform = transforms.Compose([
+        transforms.Resize(params.img_size),
+        transforms.CenterCrop(params.img_size),
+        transforms.ToTensor(),
+        utils_img.normalize_vqgan,
+    ])
+    train_loader = utils.get_dataloader(params.train_dir, vqgan_transform, params.batch_size, num_imgs=params.batch_size*params.steps, shuffle=True, num_workers=4, collate_fn=None)
+    val_loader = utils.get_dataloader(params.val_dir, vqgan_transform, params.batch_size*4, num_imgs=1000, shuffle=False, num_workers=4, collate_fn=None)
+    vqgan_to_imnet = transforms.Compose([utils_img.unnormalize_vqgan, utils_img.normalize_img])
+    
+    # Create losses
+    print(f'>>> Creating losses...')
+    print(f'Losses: {params.loss_w} and {params.loss_i}...')
+    if params.loss_w == 'mse':        
+        loss_w = lambda decoded, keys, temp=10.0: torch.mean((decoded*temp - (2*keys-1))**2) # b k - b k
+    elif params.loss_w == 'bce':
+        loss_w = lambda decoded, keys, temp=10.0: F.binary_cross_entropy_with_logits(decoded*temp, keys, reduction='mean')
+    else:
+        raise NotImplementedError
+    
+    if params.loss_i == 'mse':
+        loss_i = lambda imgs_w, imgs: torch.mean((imgs_w - imgs)**2)
+    elif params.loss_i == 'watson-dft':
+        provider = LossProvider()
+        loss_percep = provider.get_loss_function('Watson-DFT', colorspace='RGB', pretrained=True, reduction='sum')
+        loss_percep = loss_percep.to(device)
+        loss_i = lambda imgs_w, imgs: loss_percep((1+imgs_w)/2.0, (1+imgs)/2.0)/ imgs_w.shape[0]
+    elif params.loss_i == 'watson-vgg':
+        provider = LossProvider()
+        loss_percep = provider.get_loss_function('Watson-VGG', colorspace='RGB', pretrained=True, reduction='sum')
+        loss_percep = loss_percep.to(device)
+        loss_i = lambda imgs_w, imgs: loss_percep((1+imgs_w)/2.0, (1+imgs)/2.0)/ imgs_w.shape[0]
+    elif params.loss_i == 'ssim':
+        provider = LossProvider()
+        loss_percep = provider.get_loss_function('SSIM', colorspace='RGB', pretrained=True, reduction='sum')
+        loss_percep = loss_percep.to(device)
+        loss_i = lambda imgs_w, imgs: loss_percep((1+imgs_w)/2.0, (1+imgs)/2.0)/ imgs_w.shape[0]
+    else:
+        raise NotImplementedError
+
+    for ii_key in range(params.num_keys):
+
+        # Creating key
+        print(f'\n>>> Creating key with {nbit} bits...')
+        if(params.strategy==0):
+            key = torch.randint(0, 2, (1, nbit), dtype=torch.float32, device=device)
+            key_str = "".join([ str(int(ii)) for ii in key.tolist()[0]])
+        else:
+            key=torch.tensor([[1,1,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,1,0,1,0,0,1,1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,1,1,1]], dtype=torch.float32, device=device)
+            key_str="111010110101000001010111010011010100010000100111"
+        print(f'Key: {key_str}')
+
+        # Copy the LDM decoder and finetune the copy
+        ldm_decoder = deepcopy(ldm_ae)
+        ldm_decoder.encoder = nn.Identity()
+        ldm_decoder.quant_conv = nn.Identity()
+        ldm_decoder.to(device)
+        for param in ldm_decoder.parameters():
+            param.requires_grad = True
+        optim_params = utils.parse_params(params.optimizer)
+        optimizer = utils.build_optimizer(model_params=ldm_decoder.parameters(), **optim_params)
+
+        # Training loop
+        print(f'>>> Training...')
+                
+        train_stats = train(train_loader, optimizer, loss_w, loss_i, ldm_ae, ldm_decoder, msg_decoder, vqgan_to_imnet, key, params)
+        val_stats = val(val_loader, ldm_ae, ldm_decoder, msg_decoder, vqgan_to_imnet, key, params)
+        log_stats = {'key': key_str,
+                **{f'train_{k}': v for k, v in train_stats.items()},
+                **{f'val_{k}': v for k, v in val_stats.items()},
+            }
+        save_dict = {
+            'ldm_decoder': ldm_decoder.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'params': params,
+        }
+
+        # Save checkpoint
+        if(params.strategy==0):
+            torch.save(save_dict, os.path.join(params.output_dir, f"checkpoint_{ii_key:03d}.pth"))
+            with (Path(params.output_dir) / "log.txt").open("a") as f:
+                f.write(json.dumps(log_stats) + "\n")
+            with (Path(params.output_dir) / "keys.txt").open("a") as f:
+                f.write(os.path.join(params.output_dir, f"checkpoint_{ii_key:03d}.pth") + "\t" + key_str + "\n")
+            print('\n')
+        else:
+            torch.save(save_dict, os.path.join(params.output_dir, f"checkpointtar_{ii_key:03d}_{params.strategy}.pth"))
+            with (Path(params.output_dir) / "log.txt").open("a") as f:
+                f.write(json.dumps(log_stats) + "\n")
+            with (Path(params.output_dir) / "keys.txt").open("a") as f:
+                f.write(os.path.join(params.output_dir, f"checkpointtar_{ii_key:03d}_{params.strategy}.pth") + "\t" + key_str + "\n")
+            print('\n')
+def tamper_train(data_loader: Iterable, optimizer: torch.optim.Optimizer, loss_w: Callable, loss_i: Callable, ldm_ae: AutoencoderKL, ldm_decoder:AutoencoderKL, msg_decoder: nn.Module, vqgan_to_imnet:nn.Module, key: torch.Tensor, params: argparse.Namespace):
+    vqgan_transform = transforms.Compose([
+        transforms.Resize(params.img_size),
+        transforms.CenterCrop(params.img_size),
+        transforms.ToTensor(),
+        utils_img.normalize_vqgan,
+    ])
+    Dtr_loader = utils.get_dataloader(params.train_dir, vqgan_transform, params.batch_size, num_imgs=params.batch_size*params.steps, shuffle=True, num_workers=4, collate_fn=None)
+    Retain_loader = utils.get_dataloader(params.train_dir, vqgan_transform, params.batch_size, num_imgs=params.batch_size*params.steps, shuffle=True, num_workers=4, collate_fn=None)    
+    for i in range(params.outer_steps):
+        gtr=0
+        dtr_iter=iter(Dtr_loader)
+        ltr=0
+        
+        decoder=0
+        ogdecoder=deepcopy(decoder)
+        ltr_tamper_resistance_grad_scale=4.0
+        lretain_tamper_resistance_grad_scale=1.0
+        #attack optimizer initialization
+        for k in range(params.inner_steps):
+            #CALL train with strategy 1 or 2 to attack and return model which returns model with gradients
+            #attack optimizer.step as attack steps/grad accumulation steps were only 8 in TAR
+            attackeddecoder=0
+            ogmsg=key
+            ltr=(attackeddecoder(dtr_iter),ogmsg)
+            ltr*=ltr_tamper_resistance_grad_scale
+            ltr.backward()
+        #attack optimizer deletion
+        #loop for grad accumulation steps for retain loss
+        lretain=0
+        retain_iter=iter(Retain_loader)
+        lretain=(decoder(retain_iter),ogmsg)+torch.norm(decoder.hidden_States,ogdecoder.hidden_States)
+        lretain*=lretain_tamper_resistance_grad_scale
+        lretain.backward()
+        optimizer.step()
+    return decoder
+if __name__ == '__main__':
+
+    # generate parser / parse parameters
+    parser = get_parser()
+    params = parser.parse_args()
+
+    # run experiment
+    main(params)
